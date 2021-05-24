6cdcd57120728c57b3824d6ad3be4d4f843cc5f3
faisalbinafif <60437615+faisalbinafif@users.noreply.github.com>
2021-05-24 11:31:57 +0530

    website with dummy php backup

---
+++ main.js
@@ -0,0 +1,7 @@
+
+////////////////// Preloader javascript /////////////
+
+window.addEventListener("load", function () {
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden";
+});
